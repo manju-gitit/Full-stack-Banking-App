02ab8a24021b38e35f02b4d00565f3e5e4fb0a61
manju-gitit <smartsqa@gmail.com>
2023-12-07 23:03:03 -0800

    update mongo db

--- dal.js
+++ dal.js
@@ -1,16 +1,42 @@
-const MongoClient = require('mongodb').MongoClient;
+/* const MongoClient = require('mongodb').MongoClient;
 
 //const url = 'mongodb://localhost:27017';
 //const url = 'mongodb://mongo:27017';
-
-const url = "mongodb+srv://curiosity:Testing@manjus-monogdb-cluster.qvghlgy.mongodb.net/?retryWrites=true&w=majority";
+const db = null;
+ const url = "mongodb+srv://curiosity:Testing@manjus-monogdb-cluster.qvghlgy.mongodb.net/?retryWrites=true&w=majority";
 // connect to mongo
 MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
     console.log("Connected successfully to db server");
-
+    const dbName = 'manjus-app';
     // connect to myproject database
-    db = client.db('manjus-app');
+    db = client.db(dbName);
+}); */
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://curiosity:Testing@manjus-monogdb-cluster.qvghlgy.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // create user account using the collection.insertOne function
 function create(name, email, password) {
--- mongo_test.js
+++ mongo_test.js
@@ -1,6 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+/* const MongoClient = require('mongodb').MongoClient;
 //const url = 'mongodb://localhost:27017';
 const url = 'mongodb://mongo:27017';
+//const url = "mongodb+srv://curiosity:Testing@manjus-monogdb-cluster.qvghlgy.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://curiosity:<password>@manjus-monogdb-cluster.qvghlgy.mongodb.net/?retryWrites=true&w=majority";
 
 // connect to mongo
 MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
@@ -31,3 +33,29 @@ MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
             client.close();            
     });    
 });
+ */
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://curiosity:Testing@manjus-monogdb-cluster.qvghlgy.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
