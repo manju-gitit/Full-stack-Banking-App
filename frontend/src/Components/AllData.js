ab4351b1c1ca868e3d94655e141333ee42fe8624
manju-gitit <smartsqa@gmail.com>
2023-12-08 23:47:56 -0800

    all data roue change

--- frontend/src/Components/AllData.js
+++ frontend/src/Components/AllData.js
@@ -3,21 +3,18 @@ import Card from './Common/Card'
 
 function AllData() {
 const [data, setData] = useState('');
-    const baseUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001';
+    //const baseUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001';
+    const baseUrl = 'http://3.140.113.245:3001';
 
     function getAllData() {
         fetch(`${baseUrl}/account/all`)
             .then(async (res) => {
-
                 const data = await res.json();
                 return setData(data);
             })
     }
-
     useEffect(() => {
         if (data === '') { getAllData() }
-
-
     }, [data]);
 
     if (data !== '')
